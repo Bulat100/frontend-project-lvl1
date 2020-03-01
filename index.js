430d3e51d8c79f1d32fb522f45d9603978844d35
Bulat <bulat100@yandex.ru>
2020-03-01 12:10:31 +0300

    4 files added

---
+++ index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+const getName = readlineSync.question('May I have your name?');
+const greeting = () => console.log(`Hello, ${getName}!`);
+export default greeting;
---
+++ readme.md
@@ -0,0 +1,1 @@
+[![Maintainability](https://api.codeclimate.com/v1/badges/d427d960e5fca1e725b4/maintainability)](https://codeclimate.com/github/Bulat100/frontend-project-lvl1/maintainability)
